f8f1b3bc6d96b6716013190447c8a21b516d0045
Daniel Joseph Antrim <dantrim@uci.edu>
2015-08-07 09:11:48 -0700

    Restructure susyana: add tabler code
    - tabler: currently does cutflows
        > missing yields tables
        > missing systematics
        > add event lists?
        > add saving output list
        > add options for LaTeX output
        > add option to print cut efficiencies or not
    
        > add trigger acceptances (this could be separate code)

---
+++ susyana/tabler/tabler.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python
+
+from tabulate import tabulate
+
+from optparse import OptionParser
+import os
+
+import ROOT as r
+r.gROOT.SetBatch(False)
+r.gStyle.SetOptStat(False)
+import sys
+sys.path.append('../../../')
+
+r.TEventList.__init__._creates = False
+r.TH1F.__init__._creates = False
+
+import supersusy.utils.utils as utils
+import supersusy.utils.plot_utils as pu
+import supersusy.utils.background as background
+import supersusy.utils.region as region
+
+################################################
+## print the usage
+def print_usage() :
+    print "+ ----------------------------- +"
+    print "   tabler                        "
+    print "         usage                   "
+    print " "
+    print "   not sure yet                  "
+    print "+ ----------------------------- +"
+    sys.exit()
+    
+################################################
+## grab the config file
+def get_config() :
+    configuration_file = "./" + indir + "/" + config + ".py"
+    if os.path.isfile(configuration_file) :
+        return configuration_file
+    else :
+        print 'get_config ERROR    Input config ("%s") is not found in the directory/path: %s. Exitting.'%(config, configuration_file)
+        sys.exit()
+
+################################################
+## set the event lists
+def set_eventlists(regions, backgrounds, data) :
+
+    for reg in regions :
+        # set event lists, if they already exist then load them
+        print "Setting EventLists for %s"%reg.name
+        cut = ""
+        if reg.isCutFlow() : cut = reg.cutFlow[-1]
+        else : cut = reg.tcut
+
+        cut = r.TCut(cut)
+        sel = r.TCut("1")
+        for b in backgrounds :
+            list_name = "list_" + reg.name + "_" + b.treename
+            save_name = "./" + indir + "/lists/" + list_name + ".root"
+
+            # check if the list exists
+            if os.path.isfile(save_name) :
+                rfile = r.TFile.Open(save_name)
+                list = rfile.Get(list_name)
+                print "%s : EventList found at %s"%(b.name, os.path.abspath(save_name))
+                if dbg : list.Print()
+                b.tree.SetEventList(list)
+            else :
+                draw_list = ">> " + list_name
+                b.tree.Draw(draw_list, sel*cut)
+                list = r.gROOT.FindObject(list_name)
+                b.tree.SetEventList(list)
+                list.SaveAs(save_name)
+
+        if data :
+            data_list_name = "list_" + reg.name + "_" + data.treename
+            data_save_name = "./" + indir + "/lists/" + data_list_name + ".root"
+            if os.path.isfile(data_save_name) :
+                rfile = r.TFile.Open(data_save_name)
+                data_list = rfile.Get(data_list_name)
+                print "Data : EventList found at %s"%os.path.abspath(data_save_name)
+                if dbg : data_list.Print()
+                data.tree.SetEventList(data_list)
+            else :
+                draw_list = ">> " + data_list_name
+                data.tree.Draw(draw_list, sel * cut)
+                data_list = r.gROOT.FindObject(data_list_name)
+                data.tree.SetEventList(data_list)
+                data_list.SaveAs(data_save_name)
+
+################################################
+## get the yield for a bkg provided a specific cut
+def getCutYield(tcut, bkg, cutNumber) :
+    # create a canvas to prevent ROOT from telling us its doing things
+    c = r.TCanvas("c_"+b.treename+"_cutflow_"+str(cutNumber), "", 800, 600)
+    c.cd()
+
+    h = pu.th1f("h_"+b.treename+"_cutflow_"+str(cutNumber), "", 4, 0, 1,"","")
+    cut = "(" + tcut + ") * eventweight * " + str(b.scale_factor)
+    cut = r.TCut(cut)
+    sel = r.TCut("1")
+    cmd = "isMC>>%s"%h.GetName()
+    bkg.tree.Draw(cmd, cut * sel)
+
+    stat_err = r.Double(0.0)
+    integral = h.IntegralAndError(0,-1,stat_err)
+
+    h.Delete() 
+    c.Close() # prevent annoying canvas warnings
+
+    return integral
+
+################################################
+## make cutflow table
+def make_cutflow(reg, data, backgrounds) :
+    print "make_cutflow    region: %s (%s)"%(reg.name,reg.displayname)
+
+    headers = ['Cut']
+    for bkg in backgrounds :
+        headers.append(bkg.displayname) 
+    # table as a list for tabulate module
+    #  > each entry is a list and corresponds to a single row in the table (not including header)
+    #  > for each list (row), each entry is a table value at coordinate (row, column) <==> table[iRow][iColumn]
+    table = []
+
+    # tables for efficiencies
+    #   > each entry will be a list holding the efficiencies for each cut
+    #   > e.g. [ [cut1 efficiency bkg1, cut1 efficiency bkg2], [cut2 efficiency bkg1, cut2 efficiency bkg2], ... ]
+    efficiencies = []
+
+    for icut, cut in enumerate(reg.getCutFlowDict().keys()) :
+        line = []
+        line.append(cut) # first entry in the row is the name of the cut
+
+        # make a list of cut efficiencies for each background, for each cut
+        # eff_list : indices correspond to the background, values = eff. for that bkg.
+        eff_list = []
+
+        for ib, bkg in enumerate(backgrounds) :
+            # get the yield for the bkg for the cutflow up to this point
+            bkgYield = getCutYield(reg.getCutFlowList()[icut], bkg, icut)
+
+            # get the background yield from the previous cut to calculate the efficiency
+            # of the current cut
+            efficiency = ""
+            if icut == 0 : efficiency = "1.00"
+            elif icut != 0 : efficiency = "%.2f"%(bkgYield / float(table[icut-1][ib+1]) * 1.0)
+            eff_list.append(efficiency)
+
+            # add an entry to this row
+            line.append("%.2f"%bkgYield)
+
+        # add the efficiencies for this cut
+        efficiencies.append(eff_list)
+        table.append(line)
+
+    # add the efficiencies to the row values by editting the current table
+    new_table = []
+    for icut, line in enumerate(table) :
+        new_line = []
+        new_line.append(line[0])
+        for iB, bkg in enumerate(backgrounds) :
+            bline = line[iB+1] 
+            bline += " (%s)"%(efficiencies[icut][iB])
+            new_line.append(bline)
+        new_table.append(new_line)
+    table = new_table
+            
+    print tabulate(table, headers, tablefmt="rst", numalign="right", stralign="left", floatfmt=".2f")
+
+################################################
+## make the tables
+def make_tables(regions, data, backgrounds) :
+
+    # put signal samples at the end
+    ordered_backgrounds = []
+    for b in backgrounds :
+        if not b.isSignal() : ordered_backgrounds.append(b)
+    for b in backgrounds :
+        if b.isSignal() : ordered_backgrounds.append(b)
+    backgrounds = ordered_backgrounds
+
+    for reg in regions :
+        if reg.isCutFlow() : make_cutflow(reg, data, backgrounds)
+
+################################################
+## main
+if __name__=="__main__" :
+
+    # just make these global so we don't have to move them around everywhere
+    global indir, config, doSys, dbg
+
+    parser = OptionParser()
+    parser.add_option("-i", "--indir",  dest="indir", default="") 
+    parser.add_option("-c", "--config", dest="config", default="")
+    parser.add_option("-s", "--doSys",  action="store_true", dest="doSys", default=False)
+    parser.add_option("-d", "--dbg",    action="store_true", dest="dbg", default=False)
+    parser.add_option("-u", "--usage",  action="store_true", dest="usage", default=False)
+    (options, args) = parser.parse_args()
+    indir  = options.indir
+    config = options.config
+    doSys  = options.doSys
+    dbg    = options.dbg
+    usage  = options.usage
+    if usage : print_usage()
+
+    print "+ ----------------------------- +"
+    print "   tabler                        "
+    print "         input options           "
+    print "   input directory : %s          "%indir
+    print "   config          : %s          "%config
+    print "   systematics     : %s          "%doSys
+    print "   debug           : %s          "%dbg
+    print "+ ----------------------------- +"
+
+    # get the config file
+    conf_file = get_config() 
+
+    # container for the regions
+    regions = []
+    # container for the backgrounds/samples
+    backgrounds = []
+    data = None
+
+    # container for systematics (if any)
+    systematics = []
+
+    # load the config
+    execfile(conf_file) 
+
+    # print out the loaded backgrounds
+    print "+ ----------------------------- +"
+    print "   Loaded backgrounds:           "
+    for b in backgrounds :
+        b.Print()
+    if data :
+        print "   Loaded data sample:       "
+        print data.Print()    
+    print "+ ----------------------------- +"
+    if doSys :
+        print "+ ----------------------------- +"
+        print "   Loaded systematics:           "
+        for s in systematics :
+            s.check()
+            s.Print()
+            for b in backgrounds :
+                b.addSys(s)
+                if dbg :
+                    for s in b.systList :
+                        print s.tree
+        print "+ ----------------------------- +"
+
+    # set event lists
+    #set_eventlists(regions, backgrounds, data)
+
+    # make tables
+    make_tables(regions, data, backgrounds)
+
+ 
---
+++ susyana/tabler/test/testConfig.py
@@ -0,0 +1,86 @@
+import sys
+sys.path.append('../../../../../')
+import supersusy.utils.background as background
+import supersusy.utils.signal as signal
+import supersusy.utils.region as region
+
+
+############################################################
+## set up the samples
+############################################################
+
+##
+### directories
+##
+scratchdir = "/scratch/dantrim/n0211/"
+datafile = scratchdir + "data15_13TeV.root"
+mcfile = scratchdir + "sigOpt_mc15_noPRW_RF.root"
+
+backgrounds = []
+
+## ttbar
+ttbar = background.Background("ttbar", "t#bar{t}")
+ttbar.set_debug()
+ttbar.set_file(mcfile)
+ttbar.scale_factor = 1.0
+ttbar.set_treename("TTbar_CENTRAL")
+ttbar.set_merged_tree(ttbar.treename)
+backgrounds.append(ttbar)
+
+
+## bwn (250, 160)
+bwn1 = background.Background("bwn_250_160", "(250, 160)")
+bwn1.setSignal()
+bwn1.set_debug()
+bwn1.set_file(mcfile)
+bwn1.scale_factor = 1.0
+bwn1.set_treename("bwn_250_160_CENTRAL")
+bwn1.set_merged_tree(bwn1.treename)
+backgrounds.append(bwn1)
+
+## bwn (300, 180)
+bwn2 = background.Background("bwn_300_180", "(300, 180)")
+bwn2.setSignal()
+bwn2.set_debug()
+bwn2.set_file(mcfile)
+bwn2.scale_factor = 1.0
+bwn2.set_treename("bwn_300_180_CENTRAL")
+bwn2.set_merged_tree(bwn2.treename)
+backgrounds.append(bwn2)
+
+## ww
+ww = background.Background("ww", "WW")
+ww.set_debug()
+ww.set_file(mcfile)
+ww.scale_factor = 1.0
+ww.set_treename("WW_CENTRAL")
+ww.set_merged_tree(ww.treename)
+backgrounds.append(ww)
+
+############################################################
+# Set up the regions
+############################################################
+regions = []
+
+##
+### wwLoose
+##
+
+reg = region.Region("wwLoose", "wwLoose")
+reg.setCutFlow()
+reg.addCut("Exactly 2 leptons", "nLeptons==2")
+reg.addCut("DF", "nElectrons==1 && nMuons==1")
+reg.addCut("OS", "(l_q[0]*l_q[1])<0")
+reg.addCut("Lepton pT > 20 GeV", "l_pt[0]>20 && l_pt[1]>20")
+regions.append(reg)
+
+reg = region.Region("wwLooseSF", "wwLooseSF")
+reg.setCutFlow()
+reg.addCut("Exactly 2 leptons", "nLeptons==2")
+reg.addCut("SF", "(nElectrons==2 || nMuons==2)")
+reg.addCut("OS", "(l_q[0]*l_q[1])<0")
+reg.addCut("Lepton pT > 20 GeV", "l_pt[0]>20 && l_pt[1]>20")
+regions.append(reg)
+
+
+
