7e4b5b8f0692b5c9781672026e42a6edd2223b50
Daniel Joseph Antrim <dantrim@uci.edu>
2016-03-01 12:50:52 -0800

    validation plots

--- susyana/plotter/validation/valplots.py
+++ susyana/plotter/validation/valplots.py
@@ -10,19 +10,19 @@ import supersusy.utils.region as region
 #############################################
 # Set up the samples
 #############################################
-rawdir = "/data/uclhc/uci/user/dantrim/ntuples/n0216/validation/mc/Raw/"
-data_rawdir = "/data/uclhc/uci/user/dantrim/ntuples/n0216/validation/data/Raw/"
-filelist_dir = "/data/uclhc/uci/user/dantrim/n0216val/filelists/"
+rawdir = "/gdata/atlas/dantrim/SusyAna/ntuples/n0220/rj/mc/Raw/"
+data_rawdir = "/gdata/atlas/dantrim/SusyAna/ntuples/n0220/rj/data/Raw/"
+filelist_dir = "/data7/atlas/dantrim/SusyAna/n0220val/SuperRest/run/filelists/"
 backgrounds = []
 
 #### MC
 # Zjets
-zjets = background.Background("zjets", "Z+jets (PowHeg)")
+zjets = background.Background("zjets", "Z+jets (Sherpa)")
 zjets.set_debug()
 zjets.scale_factor = 1
 zjets.set_color(r.TColor.GetColor("#82DE68"))
-zjets.set_treename("Zjets_powheg")
-zjets.set_chain_from_list_CONDOR(filelist_dir + "zjets_powheg/", rawdir)
+zjets.set_treename("Zjets_sherpa")
+zjets.set_chain_from_list(filelist_dir + "zjets_sherpa.txt", rawdir)
 backgrounds.append(zjets)
 
 ttbar = background.Background("ttbar", "TTbar")
@@ -30,7 +30,7 @@ ttbar.set_debug()
 ttbar.scale_factor = 1.0
 ttbar.set_color(r.TColor.GetColor("#E67067"))
 ttbar.set_treename("ttbar_powheg")
-ttbar.set_chain_from_list_CONDOR(filelist_dir + "ttbar/", rawdir)
+ttbar.set_chain_from_list(filelist_dir + "ttbar.txt", rawdir)
 backgrounds.append(ttbar)
 
 # singletop
@@ -39,7 +39,7 @@ stop.set_debug()
 stop.scale_factor = 1.0
 stop.set_color(r.TColor.GetColor("#DE080C"))
 stop.set_treename("ST")
-stop.set_chain_from_list_CONDOR(filelist_dir + "singletop/", rawdir)
+stop.set_chain_from_list(filelist_dir + "singletop.txt", rawdir)
 backgrounds.append(stop)
 
 # wjets
@@ -48,7 +48,7 @@ wjets.set_debug()
 wjets.scale_factor = 1.0
 wjets.set_color(r.TColor.GetColor("#5E9AD6"))
 wjets.set_treename("Wjets_powheg")
-wjets.set_chain_from_list_CONDOR(filelist_dir + "wjets_powheg/", rawdir)
+wjets.set_chain_from_list(filelist_dir + "wjets_powheg.txt", rawdir)
 backgrounds.append(wjets)
 
 #diboson
@@ -57,7 +57,7 @@ diboson.set_debug()
 diboson.scale_factor = 1.0
 diboson.set_color(r.TColor.GetColor("#315E88"))
 diboson.set_treename("VV_sherpa")
-diboson.set_chain_from_list_CONDOR(filelist_dir + "diboson_sherpa/", rawdir)
+diboson.set_chain_from_list(filelist_dir + "diboson_sherpa.txt", rawdir)
 backgrounds.append(diboson)
 
 ## ww
@@ -91,7 +91,7 @@ backgrounds.append(diboson)
 data = background.Data()
 data.set_color(r.kBlack)
 data.set_treename("Data")
-data.set_chain_from_list_CONDOR(filelist_dir + "data/", data_rawdir)
+data.set_chain_from_list(filelist_dir + "n0220_data.txt", data_rawdir)
 
 
 #############################################
@@ -106,9 +106,9 @@ regions.append(reg)
 
 
 reg = region.Region()
-reg.simplename = "zpeak_ee_0j"
-reg.displayname = "Z #rightarrow ee (nJ==0 nB=0)"
-reg.tcut = "nLeptons==2 && nElectrons==2 && nSJets==0 && nBJets==0 && l_pt[0]>25 && l_pt[1]>20 && (l_q[0]*l_q[1])<1 && abs(mll-91.2)<20"
+reg.simplename = "zpeak_mm_0j"
+reg.displayname = "Z #rightarrow #mu#mu (nJ==0 nB=0)"
+reg.tcut = "nLeptons==2 && nMuons==2 && nSJets==0 && nBJets==0 && l_pt[0]>25 && l_pt[1]>20 && (l_q[0]*l_q[1])<1 && abs(mll-91.2)<20"
 regions.append(reg)
 
 reg = region.Region()
@@ -118,10 +118,23 @@ reg.tcut = "nLeptons>=2"
 regions.append(reg)
 
 reg = region.Region()
-reg.simplename = "check_2OS20"
-reg.displayname = "2OS20"
-reg.tcut == "nLeptons>=2 && (l_q[0]*l_q[1])<0 && l_pt[0]>20 && l_pt[1]>20"
+reg.simplename = "wwpre"
+reg.displayname = "WW-Pre (DFOS)"
+reg.tcut = "nLeptons==2 && nMuons==1 && nElectrons==1 && nBJets==0 && nJets>=2 && l_pt[0]>20 && l_pt[1]>20 && (l_q[0]*l_q[1]})<1"
+
+reg = region.Region()
+reg.simplename = "ttbarValZ"
+reg.displayname = "t#bar{t} val"
+reg.tcut = "nLeptons==2 && nElectrons==1 && nMuons==1 && (l_q[0]*l_q[1])<1 && nBJets>0 && l_pt[0]>20 && l_pt[1]>20"
 regions.append(reg)
+
+
+reg = region.Region()
+reg.simplename = "vvVal"
+reg.displayname = "VV val"
+reg.tcut = "nLeptons==2 && nElectrons==1 && nMuons==1 && (l_q[0]*l_q[1])<1 && nBJets==0 && nSJets==0 && l_pt[0]>15 && l_pt[1]>10 && mt2>30"
+regions.append(reg)
+
 
 #############################################
 # Set up the plots
@@ -130,138 +143,186 @@ regions.append(reg)
 plots = []
 
 #### check_2OS20
-logy = 10000000
+logy = 100000000
+logy = 1100
+p = plot.Plot1D()
+p.initialize("vvVal", "mt2", "vvVal_mt2")
+p.labels(x="m_{t2} [GeV]", y = "Entries / 5 GeV")
+p.xax(5, 0, 110)
+p.yax(0., 1200)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "met", "check_2OS20_met")
+p.initialize("vvVal", "mll", "vvVal_mll")
+p.labels(x="dilepton invariant mass [GeV]", y = "Entries / 2 GeV")
+if "ttbar" in p.name or "vv" in p.name:
+    p.xax(20, 40, 400)
+else :
+    p.xax(2, 80, 110)
+p.yax(0., 800)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
+
+p = plot.Plot1D()
+p.initialize("vvVal", "met", "vvVal_met")
 p.labels(x="TST #slash{E}_{T} [GeV]", y="Entries / 10 GeV")
 p.xax(10,0,200)
-p.yax(0.1,10000000)
-p.doLogY = True
+p.yax(0., 800 )
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 ## nVtx
 p = plot.Plot1D()
-p.initialize("check_2OS20", "nVtx", "check_2OS20_nVtx")
+p.initialize("vvVal", "nVtx", "vvVal_nVtx")
 p.labels(x="nVtx", y = "Entries / 1")
-p.xax(1, 0, 45)
-p.yax(0.1, logy)
-p.doLogY = True
+p.xax(1, 0, 22)
+p.yax(0., 500)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 ## mu
 p = plot.Plot1D()
-p.initialize("check_2OS20", "avgMu", "check_2OS20_avgMu")
+p.initialize("vvVal", "avgMu", "vvVal_avgMu")
 p.labels(x="<#mu>", y = "Entries / 2")
 p.xax(2, 0, 50)
-p.yax(0.1, logy)
-p.doLogY = True
+p.yax(0., logy)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 ## leptons
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_pt[0]", "check_2OS20_lpt0")
+p.initialize("vvVal", "l_pt[0]", "vvVal_lpt0")
 p.labels(x="lead lepton pt [GeV]", y = "Entries / 10 GeV")
-p.xax(10, 0, 350)
-p.yax(0.1, logy)
-p.doLogY = True
+p.xax(5, 0, 150)
+p.yax(0., 725)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_pt[1]", "check_2OS20_lpt1")
+p.initialize("vvVal", "l_pt[1]", "vvVal_lpt1")
 p.labels(x="sub-lead lepton pt [GeV]", y = "Entries / 10 GeV")
-p.xax(10, 0, 250)
-p.yax(0.1, logy)
-p.doLogY = True
+p.xax(5, 0, 100)
+p.yax(0., 1200)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_eta[0]", "check_2OS20_leta0")
-p.labels(x="lead lepton eta", y = "Entries / 0.1")
-p.xax(0.1, -5, 5)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "l_eta[0]", "vvVal_leta0")
+p.labels(x="lead lepton eta", y = "Entries / 0.4")
+p.xax(0.4, -5, 5)
+p.yax(0., 300)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_eta[1]", "check_2OS20_leta1")
-p.labels(x="sub-lead lepton eta", y = "Entries / 0.1")
-p.xax(0.1, -5, 5)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "l_eta[1]", "vvVal_leta1")
+p.labels(x="sub-lead lepton eta", y = "Entries / 0.4")
+p.xax(0.4, -5, 5)
+p.yax(0., 300)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_phi[0]", "check_2OS20_lphi0")
-p.labels(x="lead lepton phi", y = "Entries / 0.1 rad")
-p.xax(0.1, -3.2, 3.2)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "l_phi[0]", "vvVal_lphi0")
+p.labels(x="lead lepton phi", y = "Entries / 0.4 rad")
+p.xax(0.4, -3.2, 3.2)
+p.yax(0., 300)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "l_phi[1]", "check_2OS20_lphi1")
-p.labels(x="sub-lead lepton phi", y = "Entries / 0.1 rad")
-p.xax(0.1, -3.2, 3.2)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "l_phi[1]", "vvVal_lphi1")
+p.labels(x="sub-lead lepton phi", y = "Entries / 0.4 rad")
+p.xax(0.4, -3.2, 3.2)
+p.yax(0., 300)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "pTll", "check_2OS20_pTll")
-p.labels(x="dilepton pT [GeV]", y = "Entries / 10 GeV")
-p.xax(10, 0, 500)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "pTll", "vvVal_pTll")
+p.labels(x="dilepton pT [GeV]", y = "Entries / 8 GeV")
+p.xax(8, 0, 175)
+p.yax(0., 800)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "dphi_ll", "check_2OS20_dphi_ll")
-p.labels(x="dphi_ll", y = "Entries / 0.1 rad")
-p.xax(0.1, -3.2, 3.2)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "dphi_ll", "vvVal_dphi_ll")
+p.labels(x="dphi_ll", y = "Entries / 0.4 rad")
+p.xax(0.4, -3.2, 3.2)
+p.yax(0., 300)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
-p = plot.Plot1D()
-p.initialize("check_2OS20", "mll", "check_2OS20_mll")
-p.labels(x="dilepton invariant mass [GeV]", y = "Entries / 2 GeV")
-p.xax(2, 80, 110)
-p.yax(0.1, logy)
-p.doLogY = True
-p.setRatioCanvas(p.name)
-plots.append(p)
 
 ## jets
 p = plot.Plot1D()
-p.initialize("check_2OS20", "nSJets", "check_2OS20_nSJets")
+p.initialize("vvVal", "nSJets", "vvVal_nSJets")
 p.labels(x="number of jets (non-b-tag)", y = "Entries / 1")
 p.xax(1, 0, 10)
-p.yax(0.1, logy)
-p.doLogY = True
+p.yax(0., logy)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
 p = plot.Plot1D()
-p.initialize("check_2OS20", "sj_pt[0]", "check_2OS20_sjpt0")
-p.labels(x="lead jet (non b) pt [GeV]", y = "Entires / 20 GeV")
-p.xax(20, 0, 500)
-p.yax(0.1, logy)
-p.doLogY = True
+p.initialize("vvVal", "nBJets", "vvVal_nBJets")
+p.labels(x="number of b-tagged jets", y = "Entries / 1")
+p.xax(1, 0, 10)
+p.yax(0.1,logy)
+p.doLogY = False
 p.setRatioCanvas(p.name)
 plots.append(p)
 
+p = plot.Plot1D()
+p.initialize("vvVal", "sj_pt[0]", "vvVal_sjpt0")
+p.labels(x="lead jet (non b) pt [GeV]", y = "Entires / 20 GeV")
+p.xax(20, 0, 500)
+p.yax(0., logy)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
+
+p = plot.Plot1D()
+p.initialize("vvVal", "sj_pt[1]", "vvVal_sjpt1")
+p.labels(x="sub-lead jet (non b) pt [GeV]", y = "Entires / 20 GeV")
+p.xax(20, 0, 500)
+p.yax(0., logy)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
+
+p = plot.Plot1D()
+p.initialize("vvVal", "bj_pt[0]", "vvVal_bjpt0")
+p.labels(x="lead b-jet pt [GeV]", y = "Entries / 20 GeV")
+p.xax(20, 0, 500)
+p.yax(0., logy)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
+
+p = plot.Plot1D()
+p.initialize("vvVal", "bj_pt[1]", "vvVal_bjpt1")
+p.labels(x="sub-lead b-jet pt [GeV]", y = "Entries / 20 GeV")
+p.xax(20, 0, 500)
+p.yax(0., logy)
+p.doLogY = False
+p.setRatioCanvas(p.name)
+plots.append(p)
 
 
 
